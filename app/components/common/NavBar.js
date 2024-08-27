1b58a39a1ab7bc0b65f187f52bba1e59d78cf17a
Gichuki Muchiri <collinsgichuki01@gmail.com>
2024-08-27 03:59:37 +0300

    feat: Update navbar, Add newproduct

--- app/components/common/NavBar.js
+++ app/components/common/NavBar.js
@@ -1,111 +1,309 @@
 "use client";
 
-import React from "react";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
-  Link,
-  Dropdown,
-  Input, // Import Input from NextUI
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownItem
-} from "@nextui-org/react";
-import { ShoppingCartIcon, UserIcon } from "@heroicons/react/24/outline";
+import React, { useState, useEffect } from 'react';
+import { CiSearch } from "react-icons/ci";
+import { CiUser } from "react-icons/ci";
+import { BsBag } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
 
 function NavBar() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-  const menuItems = ["Home", "Shop", "About", "Blog", "Contact"];
-  const profileItems = [
-    { key: "login", label: "Login", href: "/login" },
-    { key: "signup", label: "Sign Up", href: "/signup" },
+  const footwearItems = [
+    "Oxfords",
+    "Derby Shoes",
+    "Loafers",
+    "Brogues",
+    "Monk Straps",
+    "Chelsea Boots",
+    "Chukka Boots",
+    "Dress Boots",
+    "Casual Boots",
+    "Driving Shoes",
   ];
 
+  const accessoriesItems = [
+    "Shoe Care Products",
+    "Shoe Accessories",
+    "Sock Options",
+    "Footwear Inserts",
+    "Storage Solutions",
+    "Shoe Maintenance Tools",
+    "Footwear Bags",
+    "Shoe Care Kits",
+    "Shoe Shelves or Organizers",
+    "Gift Items",
+    "Shoe Repair Services",
+  ];
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isFootwearOpen, setIsFootwearOpen] = useState(false);
+  const [isAccessoriesOpen, setIsAccessoriesOpen] = useState(false);
+
+  const toggleFootwear = () => {
+    setIsFootwearOpen(!isFootwearOpen);
+    setIsAccessoriesOpen(false);
+  };
+
+  const toggleAccessories = () => {
+    setIsAccessoriesOpen(!isAccessoriesOpen);
+    setIsFootwearOpen(false);
+  };
+
+  // Effect to handle body scroll locking
+  useEffect(() => {
+    if (isSidebarOpen || isCartOpen || isSearchOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isSidebarOpen, isCartOpen, isSearchOpen]);
+
   return (
-    <Navbar
-      isBordered
-      isMenuOpen={isMenuOpen}
-      onMenuOpenChange={setIsMenuOpen}
-      className="bg-white dark:bg-gray-800 w-full max-w-full mx-auto px-4" // Ensure full width with no extra space
-    >
-      {/* Left Section: Logo */}
-      <NavbarContent justify="start" className="gap-4">
-        <NavbarBrand>
-          <p className="font-bold text-inherit">GM LEATHER</p>
-        </NavbarBrand>
-      </NavbarContent>
+    <div className="bg-white dark:bg-gray-800">
+      {/* Top Navbar */}
+      <div className="flex items-center justify-between h-16 px-4 md:px-8 relative">
+        {/* Toggle Button for Mobile View */}
+        <button
+          type="button"
+          className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+        >
+          <AiOutlineMenu className="w-6 h-6" />
+          <span className="sr-only">Open Menu</span>
+        </button>
 
-      {/* Center Section: Nav Links */}
-      <NavbarContent justify="center" className="hidden sm:flex gap-8">
-        {menuItems.map((item, index) => (
-          <NavbarItem key={index}>
-            <Link color="foreground" href="#">
-              {item}
-            </Link>
-          </NavbarItem>
-        ))}
-      </NavbarContent>
+        <div className="flex items-center space-x-4">
+          <div className="relative group hidden md:block" onMouseEnter={() => setIsFootwearOpen(true)}
+            onMouseLeave={() => setIsFootwearOpen(false)}>
+            <a
+              href="#"
+              className="text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg p-2"
+              onClick={toggleFootwear}
+            >
+              FOOTWEAR
+            </a>
+            {/* Dropdown List for Desktop View */}
+            {(isFootwearOpen || isAccessoriesOpen === false) && (
+              <div className="fixed top-16 left-0 right-0 w-screen bg-white z-50 md:group-hover:block md:hidden">
+                <ul className="py-2">
+                  {footwearItems.map((item, index) => (
+                    <li
+                      key={index}
+                      className="px-4 py-2 text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 flex items-center justify-start"
+                    >
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+          <div className="relative group hidden md:block" onMouseEnter={() => setIsAccessoriesOpen(true)}
+            onMouseLeave={() => setIsAccessoriesOpen(false)}>
+            <a
+              href="#"
+              className="text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg p-2"
+              onClick={toggleAccessories}
+            >
+              ACCESSORIES
+            </a>
+            {/* Dropdown List for Desktop View */}
+            {(isAccessoriesOpen || isFootwearOpen === false) && (
+              <div className="fixed top-16 left-0 right-0 w-screen bg-white z-50 md:group-hover:block md:hidden">
+                <ul className="py-2">
+                  {accessoriesItems.map((item, index) => (
+                    <li
+                      key={index}
+                      className="px-4 py-2 text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 flex items-center justify-start"
+                    >
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
 
-      {/* Right Section: Search Bar, Cart, Profile */}
-      <NavbarContent justify="end" className="gap-4">
-        <NavbarItem className='rounded-none'>
-          <Input
-            classNames={{
-              base: "max-w-full sm:max-w-[12rem] h-10",
-              mainWrapper: "h-full",
-              input: "text-small text-gray-700 dark:text-white placeholder:text-gray-500 dark:placeholder:text-gray-400",
-              inputWrapper: "h-full font-normal dark:bg-default-500/20",
-              border: 'rounded-none'
-            }}
-            className='rounded-none'
+        {/* Centered Brand */}
+        <div className='flex flex-col items-center font-extrabold flex-grow'>
+          <h5 className="text-sm">MIRICO RICOLI</h5>
+          <span className="text-sm">Men's wear</span>
+        </div>
+
+       {/* Right Side Icons */}
+       <div className="flex items-center space-x-4">
+          <button
+            type="button"
+            className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+            onClick={() => setIsSearchOpen(!isSearchOpen)}
+          >
+            <CiSearch className="w-6 h-6" />
+            <span className="sr-only">Search</span>
+          </button>
+          <CiUser className="hidden md:block w-6 h-6" />
+          <button
+            type="button"
+            className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+            onClick={() => setIsCartOpen(!isCartOpen)}
+          >
+            <BsBag className="w-6 h-6" />
+            <span className="sr-only">Cart</span>
+          </button>
+        </div>
+      
+      </div>
+
+      {/* Sidebar Menu for Mobile View */}
+      {isSidebarOpen && (
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-75 z-50 md:hidden">
+          <div className="relative w-full bg-white dark:bg-gray-800 h-full overflow-y-auto">
+            <button
+              className="absolute top-4 right-4 text-gray-500 dark:text-gray-400"
+              onClick={() => setIsSidebarOpen(false)}
+            >
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+            <div className='flex flex-col h-full'>
+              <ul className="flex-grow p-4 py-16 space-y-4">
+                <li>
+                  <a
+                    href="#"
+                    className="block text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg p-2"
+                    onClick={() => setIsFootwearOpen(!isFootwearOpen)}
+                  >
+                    FOOTWEAR
+                  </a>
+                  {isFootwearOpen && (
+                    <ul className="py-2">
+                      {footwearItems.map((item, index) => (
+                        <li
+                          key={index}
+                          className="px-4 py-2 text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                        >
+                          {item}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg p-2"
+                    onClick={() => setIsAccessoriesOpen(!isAccessoriesOpen)}
+                  >
+                    ACCESSORIES
+                  </a>
+                  {isAccessoriesOpen && (
+                    <ul className="py-2">
+                      {accessoriesItems.map((item, index) => (
+                        <li
+                          key={index}
+                          className="px-4 py-2 text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                        >
+                          {item}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              </ul>
+              <div className="p-4 text-gray-900 dark:text-white flex flex-col items-start">
+                <div className='border-t-1 border-gray-200 w-full flex flex-row items-center px-4 gap-4'>
+                  <CiUser className="w-6 h-6 gap-4" />
+                  <a
+                    href="/login"
+                    className="flex items-center p-4 flex-row text-sm font-medium"
+                  >
+                    Sign In / Log In
+                  </a>
+                </div>
+                <div className='border-t-1 border-gray-200 w-full flex flex-row px-4 gap-4'>
+                  <a
+                    href="/login"
+                    className="block text-sm font-medium"
+                  >
+                    About Us
+                  </a>
+                  <a
+                    href="/contact"
+                    className="block text-sm font-medium"
+                  >
+                    Contact Us
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+
+      {/* Search Dropdown for Mobile View */}
+      {isSearchOpen && (
+        <div className="fixed inset-x-0 top-16 z-10 bg-white dark:bg-gray-800 p-4 shadow-lg">
+          <input
+            type="text"
+            className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Search..."
-            size="sm"
-            type="search"
           />
-        </NavbarItem>
-        <NavbarItem>
-          <Link href="#">
-            <ShoppingCartIcon className="h-6 w-6 text-gray-700 dark:text-white" />
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Dropdown className='rounded-none'>
-            <DropdownTrigger>
-              <UserIcon className="h-6 w-6 cursor-pointer text-gray-700 dark:text-white" />
-            </DropdownTrigger>
-            <DropdownMenu aria-label="Profile actions" >
-              {profileItems.map((item) => (
-                <DropdownItem key={item.key} className='rounded-none'>
-                  <Link href={item.href}>{item.label}</Link>
-                </DropdownItem>
-              ))}
-            </DropdownMenu>
-          </Dropdown>
-        </NavbarItem>
-      </NavbarContent>
+        </div>
+      )}
+    
 
-      {/* Mobile Menu Toggle */}
-      <NavbarContent className="sm:hidden" justify="end">
-        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
-      </NavbarContent>
+      {/* Cart Sidebar */}
+      {isCartOpen && (
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-75 z-50">
+          <div className="fixed inset-y-0 right-0 w-full bg-white dark:bg-gray-800 h-full">
+            <button
+              className="absolute top-4 right-4 text-gray-500 dark:text-gray-400"
+              onClick={() => setIsCartOpen(false)}
+            >
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+            <div className="p-4">
+              <h2 className="text-xl font-bold text-gray-900 dark:text-white">Cart</h2>
+              {/* Cart content here */}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
 
-      {/* Mobile Menu */}
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link className="w-full" color="foreground" href="#">
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
-    </Navbar>
   );
 }
 
--- app/components/ui/newproducts.js
+++ app/components/ui/newproducts.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { HeartIcon, ShoppingCartIcon } from '@heroicons/react/24/outline'; // Import the icons
+import { ShoppingCartIcon } from '@heroicons/react/24/outline';
 
 const products = [
   {
     id: 1,
     name: 'Basic Tee',
     href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageSrc: '/Hero-1.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
     price: '$35',
     color: 'Black',
@@ -15,7 +15,25 @@ const products = [
     id: 2,
     name: 'Basic Tee',
     href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg',
+    imageSrc: '/Hero-2.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 3,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: '/Hero-3.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 4,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: '/Hero-3.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
     price: '$35',
     color: 'Black',
@@ -25,43 +43,48 @@ const products = [
 
 export default function NewProducts() {
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <div key={product.id} className="group">
-              <div className="aspect-h-1 relative aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <img
-                  alt={product.imageAlt}
-                  src={product.imageSrc}
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-                <p className="bottom text-sm font-medium text-gray-900">{product.price}</p>
-              </div>
-              <div className="mt-4 flex justify-between">
+    <>
+      <h1 className='flex text-center m-4 text-2xl uppercase justify-center flex-grow text-gray-800 dark:text-gray-200'>
+        Discover Elegance in Every Detail
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-4 md:p-8">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="w-full max-w-xs mx-auto p-4 bg-white border border-green-100 shadow dark:bg-gray-800 dark:border-gray-700"
+          >
+            <a href={product.href}>
+              <img
+                className="border w-full"
+                src={product.imageSrc}
+                alt={product.imageAlt}
+              />
+            </a>
+            <div className="px-4 pb-5">
+              <a href={product.href}>
+                <h5 className="text-lg md:text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                  {product.name}
+                </h5>
+              </a>
+              <div className="flex items-center justify-between">
                 <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                  <span className="text-xl md:text-2xl font-bold text-green-600">
+                    {product.price}
+                  </span>
                 </div>
-                <div className="top-2 right-2 flex space-x-2">
-                  <button className="p-2 bg-white rounded-full shadow-md hover:bg-gray-100">
-                    <HeartIcon className="h-6 w-6 text-gray-700" />
-                  </button>
-                  <button className="p-2 bg-white rounded-full shadow-md hover:bg-gray-100">
-                    <ShoppingCartIcon className="h-6 w-6 text-gray-700" />
-                  </button>
+                <div className="flex items-center justify-between gap-2">
+                  <a
+                    href={product.href}
+                    className="bg-white border border-black rounded-full p-2"
+                  >
+                    <ShoppingCartIcon className="h-5 w-5 md:h-6 md:w-6 text-gray-700" />
+                  </a>
                 </div>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    </div>
+    </>
   );
 }
